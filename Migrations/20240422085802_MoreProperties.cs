2c1af5d99447578ca1ebe86aefb0236a1b958da3
gs125 <gs214gs@gmail.com>
2024-04-22 11:21:44 +0200

    Added more properties to User Model and added migration

---
+++ Migrations/20240422085802_MoreProperties.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AllPropertiesApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class MoreProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "GenderChar",
+                table: "Users",
+                type: "char(1)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "varchar(200)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<double>(
+                name: "EnergyConsumption",
+                table: "Users",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<byte>(
+                name: "HeightInCentimeters",
+                table: "Users",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0);
+
+            migrationBuilder.AddColumn<float>(
+                name: "HeightInMeters",
+                table: "Users",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<TimeOnly>(
+                name: "TimeOfBirth",
+                table: "Users",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeOnly(0, 0, 0));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Years",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EnergyConsumption",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "HeightInCentimeters",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "HeightInMeters",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "TimeOfBirth",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Years",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "GenderChar",
+                table: "Users",
+                type: "nvarchar(1)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "char(1)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(200)");
+        }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Configuration;
 
 namespace AllPropertiesApp.Models
@@ -8,6 +9,7 @@ namespace AllPropertiesApp.Models
         public Guid Id { get; set; }
         [StringLength(100, MinimumLength = 2, ErrorMessage = "Length must be between 2 and 100 characters.")]
         public required string Name { get; set; }
+        [Column(TypeName = "varchar(200)")]
         [EmailAddress]
         public required string Email { get; set; }
         [Required]
@@ -15,10 +17,36 @@ namespace AllPropertiesApp.Models
         public decimal Money { get; set; }
         [Required]
         public DateOnly DateOfBirth { get; set; }
+        public TimeOnly TimeOfBirth { get; set; }
         [Range(0, 999, ErrorMessage = "The value must be between 0 and 999.")]
         public int FavouriteNumber { get; set; }
         public bool IsStudent { get; set; }
         [RegularExpression(@"^[MWN]$", ErrorMessage = "Value must be one of the letters: M, W or N.")]
+        [Column(TypeName = "char(1)")]
         public char GenderChar { get; set; }
+        [Height("Values of HeightInMeters and HeightInCentimeters must be consistent.")]
+        public byte HeightInCentimeters { get; set; }
+        public ushort Years { get; set; }
+        [Height("Values of HeightInMeters and HeightInCentimeters must be consistent.")]
+        public float HeightInMeters { get; set; }
+        public double EnergyConsumption { get; set; }
+    }
+
+    public class HeightAttribute : ValidationAttribute
+    {
+        public HeightAttribute(string errorMessage) : base(errorMessage) { }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var user = (User)validationContext.ObjectInstance;
+            int metersInCentimeters = (int)(user.HeightInMeters * 100);
+
+            if (metersInCentimeters != user.HeightInCentimeters)
+            {
+                return new ValidationResult(ErrorMessage);
+            }
+
+            return ValidationResult.Success;
+        }
     }
 }
